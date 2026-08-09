edef324a9a890ceb0ed0857d6bcf6009f8871d81
Nemophilist <17573880540@163.com>
2026-08-09 13:16:23 +0800

    王车易位的bug修好了

--- src/main/java/com/dev/piece/King.java
+++ src/main/java/com/dev/piece/King.java
@@ -24,9 +24,9 @@ public class King extends Piece{
             }
 
             if (moved == false) {
-                if(tarCol-2==preCol&&tarRow==preRow&&!pieceIsOnStraightLine(tarCol,tarRow)){
+                if(tarCol-2==preCol&&tarRow==preRow&&!pieceIsOnStraightLine(tarCol,tarRow)&&getHitting(preCol+2,preRow)==null){
                     for (Piece p: GamePanel.simPieces){
-                        if(p.col==preCol+3&&p.row==preRow&&p.moved==false){
+                        if(p.col==preCol+3&&p.row==preRow&&p.moved==false&&this.hittingP==null){
                             GamePanel.castle=p;
                             return true;
                         }
@@ -35,17 +35,10 @@ public class King extends Piece{
             }
 
             if (moved == false) {
-                if(tarCol+2==preCol&&tarRow==preRow&&!pieceIsOnStraightLine(tarCol,tarRow)){
-                    Piece[] ps=new Piece[2];
+                if(tarCol+2==preCol&&tarRow==preRow&&!pieceIsOnStraightLine(tarCol,tarRow)&&getHitting(preCol-2,preRow)==null&&getHitting(preCol-3,preRow)==null){
                     for (Piece p: GamePanel.simPieces){
-                        if(p.col==preCol-3&&p.row==preRow){
-                            ps[0]=p;
-                        }
-                        if(p.col==preCol-4&&p.row==preRow){
-                            ps[1]=p;
-                        }
-                        if(ps[0]==null&&ps[1]!=null&&ps[1].moved==false){
-                            GamePanel.castle=ps[1];
+                        if(p.col==preCol-4&&p.row==preRow&&p.moved==false){
+                            GamePanel.castle=p;
                             return true;
                         }
                     }
